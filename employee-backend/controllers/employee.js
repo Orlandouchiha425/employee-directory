42edc48b0bd8ec47ce7cd75019cc6b2f58508ecb
orlandouchiha425 <valadez425@gmail.com>
2022-04-07 14:39:37 -0400

    fixing to show by ID/4/7/22

--- employee-backend/controllers/employee.js
+++ employee-backend/controllers/employee.js
@@ -1,38 +1,32 @@
-const express=require('express');
-const { findById } = require('../models/Employees');
-const Employee = require('../models/Employees');
-const router=express.Router();
-
-
-//create Employee
-
-router.post('/',(req,res)=>{
-    const body=req.body;
-    Employee.create(body,(error,createdEmployee)=> {
-if(!error){
-    res.status(200).json(createdEmployee)
-}else{res.status(400).json(error)}
-    })
-})
-
-router.get('/',(req,res)=>{
-    Employee.find({},(error,foundEmployees)=>{
-if(!error){
-    res.status(200).json(foundEmployees)
-}else{res.status(400).json(error)}
-    })
-  
-})
-
-router.delete('/:id',(req,res)=>{
-   
-    Employee.findByIdAndDelete(req.params.id,(error,deletedEmployee)=>{
-        if(error){
-            res.send(error)
-        }else{
-            res.send("my name is orlando")
-        }
-    })
-})
-
-module.exports=router;
+const express = require("express");
+const Employee = require("../models/Employee");
+const router = express.Router();
+router.get("/", (req, res) => {
+  Employee.find({}, (err, foundEmployees) => {
+    if (!err) {
+      res.status(200).json(foundEmployees);
+    } else {
+      res.status(400).json(err);
+    }
+  });
+});
+router.post("/", (req, res) => {
+  const { body } = req;
+  Employee.create(body, (err, createdEmployee) => {
+    if (!err) {
+      res.status(200).json({ message: "all good ", createdEmployee });
+    } else {
+      res.status(400).json(err);
+    }
+  });
+});
+router.get("/:id", (req, res) => {
+  Employee.findById(req.params.id, (err, foundEmployee) => {
+    if (!err) {
+      res.status(200).json(foundEmployee);
+    } else {
+      res.status(400).json(err);
+    }
+  });
+});
+module.exports = router;
